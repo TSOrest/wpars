7c3f997b8a56912c7be78d9e3b6b24e62f03ba83
TSOrest <TSOrest@users.noreply.github.com>
2025-04-14 19:00:55 +0300

    Update weather-pars.php
    test

--- weather-pars.php
+++ weather-pars.php
@@ -35,7 +35,6 @@ $temp2=str_replace('В','',str_replace('/','',str_replace('<','',substr($content
 #echo '<br>img2 - "'.$img2.'"';
 #echo '<br>temp2 - "'.$temp2.'"';
 #echo '<br><hr><br></div>';
-
 ?>
 
 <html>
